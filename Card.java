b092784e81ac48a4671e088d5fc2e4ce9b6e12dc
Jiseon-Lee <43613836+Jiseon-Lee@users.noreply.github.com>
2018-12-01 19:11:24 +0900

    Add files via upload

---
+++ Card.java
@@ -0,0 +1,29 @@
+
+public class Card {
+
+	public static final int suit_size = 13;
+	public static final int A = 1;
+	public static final int J = 11;
+	public static final int Q = 12;
+	public static final int K = 13;
+	public String suit;
+	public int value;
+	
+	public Card(String s, int v)
+	{
+		suit = s;
+		value = v;
+	}
+	
+	public String getSuit()
+	{
+		return suit;
+	}
+	
+	public int getValue()
+	{
+		return value;
+	}
+	
+	
+}
