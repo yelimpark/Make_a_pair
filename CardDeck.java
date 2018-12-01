571447c6db814debb2ee597f9f39919dc701f5c4
Jiseon-Lee <43613836+Jiseon-Lee@users.noreply.github.com>
2018-12-01 19:12:58 +0900

    create carddeck

---
+++ CardDeck.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Collections;
+public class CardDeck 
+{
+	public int level;
+	private Card[] deck = new Card[4*Card.suit_size];
+	
+	private void createDeck(String suit) 
+	{
+		for(int i=0; i<Card.suit_size; i++)
+		{
+			deck[i] = new Card(suit, i+1);
+		}
+	}
+	
+	public CardDeck()
+	{
+		createDeck("spade");
+		createDeck("heart");
+		createDeck("diamond");
+		createDeck("club");
+	}
+	
+	public Card newCard()
+	{
+		ArrayList<Integer> index = new ArrayList<Integer>();
+		for(int i=0; i<104; i++)
+		{
+			index.add(i);
+		}
+		Collections.shuffle(index);
+		Card card = null;
+		card = deck[index.get(0)];
+		index.remove(0);
+		for(int i=1; i<104; i++)
+		{
+			deck[i-1] = deck [i];
+		}
+		return card;
+	}
+}
