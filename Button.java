4f8b7a84624a7890412e5e7d2ad61a4353f85385
Jiseon-Lee <43613836+Jiseon-Lee@users.noreply.github.com>
2018-12-09 00:22:40 +0900

    Button.java

--- Button.java
+++ Button.java
@@ -2,15 +2,11 @@ import javax.swing.*;
 import javax.swing.ImageIcon;
 public class Button extends JButton
 {
-	private CardBoard board;
-	private CardFrame frame;
 	ImageIcon image1, image2;
 	boolean check;
 	
-	public Button(CardBoard b, CardFrame f, ImageIcon image,ImageIcon back)
+	public Button(ImageIcon image,ImageIcon back)
 	{
-		board = b;
-		frame = f;
 		image1 = image;
 		image2 = back;
 		check = true;
