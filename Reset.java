3c1b0cf498ea2ff82632771b5416cb783001fcf7
Jiseon-Lee <43613836+Jiseon-Lee@users.noreply.github.com>
2018-12-11 10:49:05 +0900

    Reset.java

--- Reset.java
+++ Reset.java
@@ -7,11 +7,14 @@ public class Reset
 	ImageIcon[] image;
 	String suit;
 	int value;
-	int [] arr;
+	Card[] deck;
+	int imagenum;
 	
 	public Reset() 
 	{
 		image = new ImageIcon[31];
+		deck = new Card[15];
+		getimage();
 	}
 	
 	public ImageIcon GetImage(int i) 
@@ -19,6 +22,15 @@ public class Reset
 		return image[i];
 	}
 	
+	public String cardname(int i)
+	{
+		Card card = deck[i];
+		String Suit = card.getSuit();
+		int Value = card.getValue();
+		String name = Suit + Integer.toString(Value);
+		return name;
+	}
+
 	public void getimage()
 	{
 		ArrayList<Integer> index = new ArrayList<Integer>();
@@ -30,64 +42,66 @@ public class Reset
 		CardDeck cd = new CardDeck();
 		for(int i=0; i<15; i++)
 		{
-			Card card = cd.newCard();
+			Card card = new Card(" ",0);
+            card = cd.newCard();
 			suit = card.getSuit();
 			value = card.getValue();
+			deck[i] = card;
 			if(suit == "club")
 			{
 				switch (value)
 				{
 				case 1:
-					image[index.get(i)] = new ImageIcon("./cards/cA");
-					image[index.get(i+15)] = new ImageIcon("./cards/cA");
+					image[index.get(i)] = new ImageIcon("cards/cA.png");
+					image[index.get(i+15)] = new ImageIcon("cards/cA.png");
 				break;
 				case 2:
-					image[index.get(i)] = new ImageIcon("./cards/c2");
-					image[index.get(i+15)] = new ImageIcon("./cards/c2");
+					image[index.get(i)] = new ImageIcon("cards/c2.png");
+					image[index.get(i+15)] = new ImageIcon("cards/c2.png");
 				break;
 				case 3:
-					image[index.get(i)] = new ImageIcon("./cards/c3");
-					image[index.get(i+15)] = new ImageIcon("./cards/c3");
+					image[index.get(i)] = new ImageIcon("cards/c3.png");
+					image[index.get(i+15)] = new ImageIcon("cards/c3.png");
 				break;
 				case 4:
-					image[index.get(i)] = new ImageIcon("./cards/c4");
-					image[index.get(i+15)] = new ImageIcon("./cards/c4");
+					image[index.get(i)] = new ImageIcon("cards/c4.png");
+					image[index.get(i+15)] = new ImageIcon("cards/c4.png");
 				break;
 				case 5:
-					image[index.get(i)] = new ImageIcon("./cards/c5");
-					image[index.get(i+15)] = new ImageIcon("./cards/c5");
+					image[index.get(i)] = new ImageIcon("cards/c5.png");
+					image[index.get(i+15)] = new ImageIcon("cards/c5.png");
 				break;
 				case 6:
-					image[index.get(i)] = new ImageIcon("./cards/c6");
-					image[index.get(i+15)] = new ImageIcon("./cards/c6");
+					image[index.get(i)] = new ImageIcon("cards/c6.png");
+					image[index.get(i+15)] = new ImageIcon("cards/c6.png");
 				break;
 				case 7:
-					image[index.get(i)] = new ImageIcon("./cards/c7");
-					image[index.get(i+15)] = new ImageIcon("./cards/c7");
+					image[index.get(i)] = new ImageIcon("cards/c7.png");
+					image[index.get(i+15)] = new ImageIcon("cards/c7.png");
 				break;
 				case 8:
-					image[index.get(i)] = new ImageIcon("./cards/c8");
-					image[index.get(i+15)] = new ImageIcon("./cards/c8");
+					image[index.get(i)] = new ImageIcon("cards/c8.png");
+					image[index.get(i+15)] = new ImageIcon("cards/c8.png");
 				break;
 				case 9:
-					image[index.get(i)] = new ImageIcon("./cards/c9");
-					image[index.get(i+15)] = new ImageIcon("./cards/c9");
+					image[index.get(i)] = new ImageIcon("cards/c9.png");
+					image[index.get(i+15)] = new ImageIcon("cards/c9.png");
 				break;
 				case 10:
-					image[index.get(i)] = new ImageIcon("./cards/c10");
-					image[index.get(i+15)] = new ImageIcon("./cards/c10");
+					image[index.get(i)] = new ImageIcon("cards/c10.png");
+					image[index.get(i+15)] = new ImageIcon("cards/c10.png");
 				break;
 				case 11:
-					image[index.get(i)] = new ImageIcon("./cards/cJ");
-					image[index.get(i+15)] = new ImageIcon("./cards/cJ");
+					image[index.get(i)] = new ImageIcon("cards/cJ.png");
+					image[index.get(i+15)] = new ImageIcon("cards/cJ.png");
 				break;
 				case 12:
-					image[index.get(i)] = new ImageIcon("./cards/cQ");
-					image[index.get(i+15)] = new ImageIcon("./cards/cQ");
+					image[index.get(i)] = new ImageIcon("cards/cQ.png");
+					image[index.get(i+15)] = new ImageIcon("cards/cQ.png");
 				break;
 				case 13:
-					image[index.get(i)] = new ImageIcon("./cards/cK");
-					image[index.get(i+15)] = new ImageIcon("./cards/cK");
+					image[index.get(i)] = new ImageIcon("cards/cK.png");
+					image[index.get(i+15)] = new ImageIcon("cards/cK.png");
 				break;
 				}
 			}
@@ -96,56 +110,56 @@ public class Reset
 				switch (value)
 				{
 				case 1:
-					image[index.get(i)] = new ImageIcon("./cards/dA");
-					image[index.get(i+15)] = new ImageIcon("./cards/dA");
+					image[index.get(i)] = new ImageIcon("cards/dA.png");
+					image[index.get(i+15)] = new ImageIcon("cards/dA.png");
 				break;
 				case 2:
-					image[index.get(i)] = new ImageIcon("./cards/d2");
-					image[index.get(i+15)] = new ImageIcon("./cards/d2");
+					image[index.get(i)] = new ImageIcon("cards/d2.png");
+					image[index.get(i+15)] = new ImageIcon("cards/d2.png");
 				break;
 				case 3:
-					image[index.get(i)] = new ImageIcon("./cards/d3");
-					image[index.get(i+15)] = new ImageIcon("./cards/d3");
+					image[index.get(i)] = new ImageIcon("cards/d3.png");
+					image[index.get(i+15)] = new ImageIcon("cards/d3.png");
 				break;
 				case 4:
-					image[index.get(i)] = new ImageIcon("./cards/d4");
-					image[index.get(i+15)] = new ImageIcon("./cards/d4");
+					image[index.get(i)] = new ImageIcon("cards/d4.png");
+					image[index.get(i+15)] = new ImageIcon("cards/d4.png");
 				break;
 				case 5:
-					image[index.get(i)] = new ImageIcon("./cards/d5");
-					image[index.get(i+15)] = new ImageIcon("./cards/d5");
+					image[index.get(i)] = new ImageIcon("cards/d5.png");
+					image[index.get(i+15)] = new ImageIcon("cards/d5.png");
 				break;
 				case 6:
-					image[index.get(i)] = new ImageIcon("./cards/d6");
-					image[index.get(i+15)] = new ImageIcon("./cards/d6");
+					image[index.get(i)] = new ImageIcon("cards/d6.png");
+					image[index.get(i+15)] = new ImageIcon("cards/d6.png");
 				break;
 				case 7:
-					image[index.get(i)] = new ImageIcon("./cards/d7");
-					image[index.get(i+15)] = new ImageIcon("./cards/d7");
+					image[index.get(i)] = new ImageIcon("cards/d7.png");
+					image[index.get(i+15)] = new ImageIcon("cards/d7.png");
 				break;
 				case 8:
-					image[index.get(i)] = new ImageIcon("./cards/d8");
-					image[index.get(i+15)] = new ImageIcon("./cards/d8");
+					image[index.get(i)] = new ImageIcon("cards/d8.png");
+					image[index.get(i+15)] = new ImageIcon("cards/d8.png");
 				break;
 				case 9:
-					image[index.get(i)] = new ImageIcon("./cards/d9");
-					image[index.get(i+15)] = new ImageIcon("./cards/d9");
+					image[index.get(i)] = new ImageIcon("cards/d9.png");
+					image[index.get(i+15)] = new ImageIcon("cards/d9.png");
 				break;
 				case 10:
-					image[index.get(i)] = new ImageIcon("./cards/d10");
-					image[index.get(i+15)] = new ImageIcon("./cards/d10");
+					image[index.get(i)] = new ImageIcon("cards/d10.png");
+					image[index.get(i+15)] = new ImageIcon("cards/d10.png");
 				break;
 				case 11:
-					image[index.get(i)] = new ImageIcon("./cards/dJ");
-					image[index.get(i+15)] = new ImageIcon("./cards/dJ");
+					image[index.get(i)] = new ImageIcon("cards/dJ.png");
+					image[index.get(i+15)] = new ImageIcon("cards/dJ.png");
 				break;
 				case 12:
-					image[index.get(i)] = new ImageIcon("./cards/dQ");
-					image[index.get(i+15)] = new ImageIcon("./cards/dQ");
+					image[index.get(i)] = new ImageIcon("cards/dQ.png");
+					image[index.get(i+15)] = new ImageIcon("cards/dQ.png");
 				break;
 				case 13:
-					image[index.get(i)] = new ImageIcon("./cards/dK");
-					image[index.get(i+15)] = new ImageIcon("./cards/dK");
+					image[index.get(i)] = new ImageIcon("cards/dK.png");
+					image[index.get(i+15)] = new ImageIcon("cards/dK.png");
 				break;
 				}
 			}
@@ -154,56 +168,56 @@ public class Reset
 				switch (value)
 				{
 				case 1:
-					image[index.get(i)] = new ImageIcon("./cards/hA");
-					image[index.get(i+15)] = new ImageIcon("./cards/hA");
+					image[index.get(i)] = new ImageIcon("cards/hA.png");
+					image[index.get(i+15)] = new ImageIcon("cards/hA.png");
 				break;
 				case 2:
-					image[index.get(i)] = new ImageIcon("./cards/h2");
-					image[index.get(i+15)] = new ImageIcon("./cards/h2");
+					image[index.get(i)] = new ImageIcon("cards/h2.png");
+					image[index.get(i+15)] = new ImageIcon("cards/h2.png");
 				break;
 				case 3:
-					image[index.get(i)] = new ImageIcon("./cards/h3");
-					image[index.get(i+15)] = new ImageIcon("./cards/h3");
+					image[index.get(i)] = new ImageIcon("cards/h3.png");
+					image[index.get(i+15)] = new ImageIcon("cards/h3.png");
 				break;
 				case 4:
-					image[index.get(i)] = new ImageIcon("./cards/h4");
-					image[index.get(i+15)] = new ImageIcon("./cards/h4");
+					image[index.get(i)] = new ImageIcon("cards/h4.png");
+					image[index.get(i+15)] = new ImageIcon("cards/h4.png");
 				break;
 				case 5:
-					image[index.get(i)] = new ImageIcon("./cards/h5");
-					image[index.get(i+15)] = new ImageIcon("./cards/h5");
+					image[index.get(i)] = new ImageIcon("cards/h5.png");
+					image[index.get(i+15)] = new ImageIcon("cards/h5.png");
 				break;
 				case 6:
-					image[index.get(i)] = new ImageIcon("./cards/h6");
-					image[index.get(i+15)] = new ImageIcon("./cards/h6");
+					image[index.get(i)] = new ImageIcon("cards/h6.png");
+					image[index.get(i+15)] = new ImageIcon("cards/h6.png");
 				break;
 				case 7:
-					image[index.get(i)] = new ImageIcon("./cards/h7");
-					image[index.get(i+15)] = new ImageIcon("./cards/h7");
+					image[index.get(i)] = new ImageIcon("cards/h7.png");
+					image[index.get(i+15)] = new ImageIcon("cards/h7.png");
 				break;
 				case 8:
-					image[index.get(i)] = new ImageIcon("./cards/h8");
-					image[index.get(i+15)] = new ImageIcon("./cards/h8");
+					image[index.get(i)] = new ImageIcon("cards/h8.png");
+					image[index.get(i+15)] = new ImageIcon("cards/h8.png");
 				break;
 				case 9:
-					image[index.get(i)] = new ImageIcon("./cards/h9");
-					image[index.get(i+15)] = new ImageIcon("./cards/h9");
+					image[index.get(i)] = new ImageIcon("cards/h9.png");
+					image[index.get(i+15)] = new ImageIcon("cards/h9.png");
 				break;
 				case 10:
-					image[index.get(i)] = new ImageIcon("./cards/h10");
-					image[index.get(i+15)] = new ImageIcon("./cards/h10");
+					image[index.get(i)] = new ImageIcon("cards/h10.png");
+					image[index.get(i+15)] = new ImageIcon("cards/h10.png");
 				break;
 				case 11:
-					image[index.get(i)] = new ImageIcon("./cards/hJ");
-					image[index.get(i+15)] = new ImageIcon("./cards/hJ");
+					image[index.get(i)] = new ImageIcon("cards/hJ.png");
+					image[index.get(i+15)] = new ImageIcon("cards/hJ.png");
 				break;
 				case 12:
-					image[index.get(i)] = new ImageIcon("./cards/hQ");
-					image[index.get(i+15)] = new ImageIcon("./cards/hQ");
+					image[index.get(i)] = new ImageIcon("cards/hQ.png");
+					image[index.get(i+15)] = new ImageIcon("cards/hQ.png");
 				break;
 				case 13:
-					image[index.get(i)] = new ImageIcon("./cards/hK");
-					image[index.get(i+15)] = new ImageIcon("./cards/hK");
+					image[index.get(i)] = new ImageIcon("cards/hK.png");
+					image[index.get(i+15)] = new ImageIcon("cards/hK.png");
 				break;
 				}
 			}
@@ -212,60 +226,60 @@ public class Reset
 				switch (value)
 				{
 				case 1:
-					image[index.get(i)] = new ImageIcon("./cards/sA");
-					image[index.get(i+15)] = new ImageIcon("./cards/sA");
+					image[index.get(i)] = new ImageIcon("cards/sA.png");
+					image[index.get(i+15)] = new ImageIcon("cards/sA.png");
 				break;
 				case 2:
-					image[index.get(i)] = new ImageIcon("./cards/s2");
-					image[index.get(i+15)] = new ImageIcon("./cards/s2");
+					image[index.get(i)] = new ImageIcon("cards/s2.png");
+					image[index.get(i+15)] = new ImageIcon("cards/s2.png");
 				break;
 				case 3:
-					image[index.get(i)] = new ImageIcon("./cards/s3");
-					image[index.get(i+15)] = new ImageIcon("./cards/s3");
+					image[index.get(i)] = new ImageIcon("cards/s3.png");
+					image[index.get(i+15)] = new ImageIcon("cards/s3.png");
 				break;
 				case 4:
-					image[index.get(i)] = new ImageIcon("./cards/s4");
-					image[index.get(i+15)] = new ImageIcon("./cards/s4");
+					image[index.get(i)] = new ImageIcon("cards/s4.png");
+					image[index.get(i+15)] = new ImageIcon("cards/s4.png");
 				break;
 				case 5:
-					image[index.get(i)] = new ImageIcon("./cards/s5");
-					image[index.get(i+15)] = new ImageIcon("./cards/s5");
+					image[index.get(i)] = new ImageIcon("cards/s5.png");
+					image[index.get(i+15)] = new ImageIcon("cards/s5.png");
 				break;
 				case 6:
-					image[index.get(i)] = new ImageIcon("./cards/s6");
-					image[index.get(i+15)] = new ImageIcon("./cards/s6");
+					image[index.get(i)] = new ImageIcon("cards/s6.png");
+					image[index.get(i+15)] = new ImageIcon("cards/s6.png");
 				break;
 				case 7:
-					image[index.get(i)] = new ImageIcon("./cards/s7");
-					image[index.get(i+15)] = new ImageIcon("./cards/s7");
+					image[index.get(i)] = new ImageIcon("cards/s7.png");
+					image[index.get(i+15)] = new ImageIcon("cards/s7.png");
 				break;
 				case 8:
-					image[index.get(i)] = new ImageIcon("./cards/s8");
-					image[index.get(i+15)] = new ImageIcon("./cards/s8");
+					image[index.get(i)] = new ImageIcon("cards/s8.png");
+					image[index.get(i+15)] = new ImageIcon("cards/s8.png");
 				break;
 				case 9:
-					image[index.get(i)] = new ImageIcon("./cards/s9");
-					image[index.get(i+15)] = new ImageIcon("./cards/s9");
+					image[index.get(i)] = new ImageIcon("cards/s9.png");
+					image[index.get(i+15)] = new ImageIcon("cards/s9.png");
 				break;
 				case 10:
-					image[index.get(i)] = new ImageIcon("./cards/s10");
-					image[index.get(i+15)] = new ImageIcon("./cards/s10");
+					image[index.get(i)] = new ImageIcon("cards/s10.png");
+					image[index.get(i+15)] = new ImageIcon("cards/s10.png");
 				break;
 				case 11:
-					image[index.get(i)] = new ImageIcon("./cards/sJ");
-					image[index.get(i+15)] = new ImageIcon("./cards/sJ");
+					image[index.get(i)] = new ImageIcon("cards/sJ.png");
+					image[index.get(i+15)] = new ImageIcon("cards/sJ.png");
 				break;
 				case 12:
-					image[index.get(i)] = new ImageIcon("./cards/sQ");
-					image[index.get(i+15)] = new ImageIcon("./cards/sQ");
+					image[index.get(i)] = new ImageIcon("cards/sQ.png");
+					image[index.get(i+15)] = new ImageIcon("cards/sQ.png");
 				break;
 				case 13:
-					image[index.get(i)] = new ImageIcon("./cards/sK");
-					image[index.get(i+15)] = new ImageIcon("./cards/sK");
+					image[index.get(i)] = new ImageIcon("cards/sK.png");
+					image[index.get(i+15)] = new ImageIcon("cards/sK.png");
 				break;
 				}
 			}
 		}
-		image[30] = new ImageIcon("./cards/back");
+		image[30] = new ImageIcon("cards/back.png");
 	}	
 }
