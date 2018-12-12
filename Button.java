af4ef7dbf9c3d9f1e78e9c6b5dac27e20d732ce6
yelimpark <43175834+yelimpark@users.noreply.github.com>
2018-12-12 23:19:25 +0900

    Update Button.java

--- Button.java
+++ Button.java
@@ -1,91 +1,51 @@
-package game;
-import java.awt.event.*;
+package project;
+
 import javax.swing.*;
-public class Button extends JButton implements ActionListener
+import javax.swing.ImageIcon;
+
+public class Button extends JButton
 {
-	ImageIcon image1, image2;
-	boolean check;  // true = 뒷면  false = 앞면 
-	int i = 0; //선택한 카드 번째수 
-	Button[] btn = new Button[2];  // 버튼 
-	String name;  //카드 이름 
-	String[] checkarr = new String[2]; //2개의 카드 이름 배열 
-	int score = 0; //맞춘 카드 쌍 개수 
-	int num;
+	private ImageIcon front, back;
+	private boolean backCheck;  // true = 뒷면  false = 앞면  
+	private String name;  //카드 이름 
 	
-	
-	public Button(String n, ImageIcon image,ImageIcon back)
+	public Button(String n, ImageIcon InFront,ImageIcon Inback)
 	{
-		image1 = image;
-		image2 = back;
-		check = true;
+		front = InFront;
+		back = Inback;
+		backCheck = true;
 		name = n;
-		returnImage();
-		addActionListener(this);
+		setIcon(back);
 	}
 	
-	public String clickButton() // 앞면 보여주기 
+	public String getName() {return name;}
+	
+	public boolean getBackcheck() {return backCheck;}
+	
+	public void turnImage() // 카드뒤집기
 	{
-		if(check)
+		if (backCheck)
 		{
-			setIcon(image1);
-			check=false;
-			return name;
-		}
-		return null;
-	}
-	
-	public void returnImage() // 뒷면 보여주기 
-	{
-		setIcon(image2);
-		check=true;
-	}
-	
-	public void cardcheck() // 카드가 같은지 체크 
-	{
-		if(checkarr[0] == checkarr[1])
-		{
-			score++;
-			i = 0;
+			setIcon(front);
+			backCheck=false;
 		}
 		else
 		{
-			try 
-			{
-				Thread.sleep(1000);
-			} 
-			catch (InterruptedException e1) 
-			{
-				e1.printStackTrace();
-			}
-			btn[num].returnImage();
-			btn[num].returnImage();
-			i = 0;
+			setIcon(back);
+			backCheck=true;
 		}
 	}
 	
-	public boolean scorecheck()  //전부 맞췄는지 
+	public boolean cardcheck(Button other) // 카드가 같은지 체크 
 	{
-		if(score == 15)
+		if(this.name == other.name)
+		{
 			return true;
-		else
-			return false;
-	}
-	
-	public void actionPerformed(ActionEvent e)
-	{
-		btn[num] = (Button)e.getSource();
-		checkarr[i] = btn[num].clickButton();
-		
-		if(checkarr[i] == null)
-			return;
-		if(i==1)
-		{
-			cardcheck();
-			boolean gameend = scorecheck();
-			if(gameend)
-				return;//끝났을 때 상황 
 		}
-		i++;
+		else
+		{
+			return false;
+		}
 	}
+
 }
-	
