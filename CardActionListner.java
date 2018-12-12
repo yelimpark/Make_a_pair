96a07dbbca917b8ac7eb72f5a07eecb0b5c66065
yelimpark <43175834+yelimpark@users.noreply.github.com>
2018-12-12 23:21:33 +0900

    Update CardActionListner.java

--- CardActionListner.java
+++ CardActionListner.java
@@ -1,8 +1,6 @@
 package project;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
+import java.awt.event.*;
 import javax.swing.JOptionPane;
 
 import project.Button;
@@ -11,12 +9,32 @@ import project.LevelFrame;
 
 public class CardActionListner implements ActionListener
 {
-	Button[] btn = new Button[2];  // 버튼 
-	private int num = 0; // 선택수-1
-	int score = 0; //맞춘 카드 쌍 개수 
-	boolean sameCard = false;
+	// 외부에서 가져올것 
+	private CardFrame view;
+	private User user;
+	private UserList UL;
+	private int price;
 	
-	public boolean scorecheck()  //전부 맞췄는지 
+	//내부에서만 ㅆ는것
+	private Button[] btn = new Button[2];  // 버튼 저장해둘 리스트
+	private int num = 0; // 선택수-1
+	private int score = 0; //맞춘 카드 쌍 개수 
+	private boolean sameCard = false;
+	
+	CardActionListner(CardFrame v)
+	{
+		view = v;
+	}
+	
+	public void BringData(CardFrame v, User u, UserList ul,int p) //필요한 정보 가져오기
+	{
+		view = v;
+		user = u;
+		UL = ul;
+		price = p;
+	}
+	
+	public boolean scorecheck()  //전부 맞췄는지 검사
 	{
 		if(score == 15)
 			return true;
@@ -27,25 +45,44 @@ public class CardActionListner implements ActionListener
 	public void actionPerformed(ActionEvent e)
 	{
 		btn[num] = (Button)e.getSource(); // 버튼의 num번째에 누른 버튼 넣기
-		btn[num].turnImage();
 		
-		if (num == 1)
+		if (!btn[num].getBackcheck()) // 누른 버튼이 앞면일때
 		{
-			sameCard = btn[num].cardcheck(btn[0]);
-			num = 0;
 			btn = new Button[2];
 		}
-		
-		if (sameCard)
-			score++;
-		
-		if (scorecheck())
+		else
 		{
-			JOptionPane.showMessageDialog(null,"축하합니다! 승리하셨습니다!\n"
-					+ "point +" + String.valueOf(price));
-			setVisible(false);
+			btn[num].turnImage();
+			
+			if (num == 1)
+			{
+				sameCard = btn[num].cardcheck(btn[0]);
+				num = 0;
+				btn = new Button[2];
+			}
+			
+			if (sameCard)
+				score++;
+			else
+			{
+				btn[0].turnImage();
+				btn[1].turnImage();
+			}
+			
+			if (scorecheck())
+			{
+				JOptionPane.showMessageDialog(null,"축하합니다! 승리하셨습니다!\n"
+						+ "point +" + String.valueOf(price));
+				user.ChangePoint(price);
+				view.setVisible(false);
+				LevelFrame Flevel= new LevelFrame();
+				Flevel.BringData(user,UL);
+				Flevel.setVisible(true);
+				view.dispose();
+			}
+			
+			num++; // num 증가
 		}
-		num++; // num 증가
 	}
 	
 }
