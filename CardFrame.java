2ca90aa06f5114e372806e56c9378f64497bd426
Jiseon-Lee <43613836+Jiseon-Lee@users.noreply.github.com>
2018-12-14 22:17:31 +0900

    CardFrame.java
    add timer

--- CardFrame.java
+++ CardFrame.java
@@ -1,53 +1,43 @@
-package project;
-
 import javax.swing.*;
-
-import project.Button;
-import project.Reset;
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class CardFrame extends JFrame
 {
-	//Reset Game;
 	private int level = 0,price = level*2; //(ex / 쉬움 = 1, price =2)
 	private User user = new User();
 	private JLabel id,point; // 외부에서 지정
-	private UserList UL;
+	private UserList UL = new UserList();
 	private CardActionListner CA = new CardActionListner(this); //카드만 포함
-	private JButton item_1,item_2,quit;
+	private JButton item,item_2,quit;
+	private JLabel time = new JLabel();
+	private timer timer = new timer(40, this); //40은 나중에 고칠 수 있음 레벨에 따라 다르게 나타나기 등으로 
 	
 	public CardFrame()
 	{
-		//Game=new Reset();
 		Button[] button = new Button[30];
 		setLayout(null);
-		
-		JLabel time = new JLabel("시간"); // 시간 나타내기 
-		time.setBounds(50,20,80,30);
+ 
+		time.setText("시간 : "+timer.gettime());  //시간 문구 바꾸
+		time.setBounds(50,20,80,30); 
 		add(time);
 
 		id = new JLabel("id"); // 사용자 아아디 나타내기 
-		id.setBounds(770,20,80,30);
+		id.setBounds(740,20,80,30);
 		add(id);
 		point = new JLabel("point");
 		add(point);
 		point.setBounds(820,20,80,30);
 		
-		JPanel cardb = new JPanel(); // 카드 배열 패널에 추가 
-		cardb.setBounds(50,80,800,620);
-		cardb.setLayout(new GridLayout(5,6));
-		
 		JPanel cardf = new JPanel();
 		cardf.setBounds(50,80,800,620);
-		cardf.setLayout(new GridLayout(5,6));
+		cardf.setLayout(new GridLayout(5,6,30,0));
 		
 		Reset Game = new Reset();
-		for (int i=0; i<30; i++)
+		for (int i=0; i<30; i++)  //버튼 넣기  
 		{
-			button[i] = new Button(Game.cardname(i), Game.GetImage(i), Game.GetImage(30));
+			button[i] = new Button(Game.cardname(i), Game.GetImage(30), Game.GetImage(i));
 			cardf.add(button[i]);
 			button[i].addActionListener(CA);
 		}
@@ -55,15 +45,10 @@ public class CardFrame extends JFrame
 		
 		CardFrameListner CFL = new CardFrameListner();
 		
-		item_1 = new JButton(); // 아이템1
-		item_1.setBounds(880,80,90,90);
-		add(item_1);
-		item_1.addActionListener(CFL);
-		
-		item_2 = new JButton("item"); // 아이템1
-		item_2.setBounds(880,200,90,90);
-		add(item_2);
-		item_2.addActionListener(CFL);
+		item = new JButton("item"); // 아이템1
+		item.setBounds(880,80,90,90);
+		add(item);
+		item.addActionListener(CFL);
 		
 		quit = new JButton("item");  // 나가기 
 		quit.setBounds(880,650,90,50);
@@ -73,8 +58,13 @@ public class CardFrame extends JFrame
 		setSize(1000, 750);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("test");
-		
 	}
+	
+	public void update()
+	{
+		time.setText("시간 : "+timer.gettime());
+	}
+	
 	
 	public void BringData(User arguser,UserList argUL,int arglevel) //로그인창에서 필요한 정보 가져오기
 	{
@@ -83,16 +73,16 @@ public class CardFrame extends JFrame
 		UL = argUL;
 		id.setText("ID : " + user.Getid());
 		point.setText(String.valueOf("POINT : " + user.GetPoint()));
-		CA.BringData(this,user,UL,price);
+		CA.BringData(user,UL,price);
 	}
 	
 	public class CardFrameListner implements ActionListener 
 	{
 		public void actionPerformed(ActionEvent e)
 		{
-			if (e.getSource() == item_1)
+			if (e.getSource() == item)
 			{
-				
+				timer.timeadd(5);
 			}
 			else if (e.getSource() == item_2)
 			{
@@ -108,11 +98,5 @@ public class CardFrame extends JFrame
 				dispose();
 			}
 		}
-	}
-	
-	public static void main(String[] args)
-	{
-	//CardFrame f = new CardFrame();
-	//f.setVisible(true);
 	}
 }
